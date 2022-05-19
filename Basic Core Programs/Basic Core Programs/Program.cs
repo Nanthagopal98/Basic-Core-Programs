ae4df3fa65f3f7f20fcfefa872e9469bfd2d0b37
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-19 12:55:44 +0530

    [Nantha] - Prime factors completed and added

--- Basic Core Programs/Basic Core Programs/Program.cs
+++ Basic Core Programs/Basic Core Programs/Program.cs
@@ -3,6 +3,7 @@ Console.WriteLine("Enter 1 to run flip Coin");
 Console.WriteLine("Enter 2 to run leap Year");
 Console.WriteLine("Enter 3 to run power of two program");
 Console.WriteLine("Enter 4 to run harmonic Value");
+Console.WriteLine("Enter 5 to run prime Factors");
 int value = Convert.ToInt32(Console.ReadLine());
 
 
@@ -23,6 +24,10 @@ switch (value)
     case 4:
         Basic_Core_Programs.harmonicValue run4 = new Basic_Core_Programs.harmonicValue();
         run4.checkHarmonicValue();
+        break;
+    case 5:
+        Basic_Core_Programs.primeFactors run5 = new Basic_Core_Programs.primeFactors();
+        run5.findPrimeFactors();
         break;
 }
 
---
+++ Basic Core Programs/Basic Core Programs/primeFactors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    public class primeFactors
+    {
+        public void findPrimeFactors()
+        {
+            List<int> factors = new List<int>();
+            int j = 0;
+            Console.WriteLine("Enter value to find Prime Factor");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Primr factors for " + input);
+            for (int i = 2; i <= input;)
+            {
+                var check = (input % i);
+                if (check == 0)
+                {
+                    input = input / i;
+                    factors.Add(i);
+                    j++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            for (int n = 0; n < j; n++)
+            {
+                Console.WriteLine(factors[n]);
+            }
+        }
+    }
+}
