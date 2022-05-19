08a930300661c80aadda8f1fdbea062d46d7b740
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-19 12:29:59 +0530

    [Nantha] - leapYear completed

--- Basic Core Programs/Basic Core Programs/Program.cs
+++ Basic Core Programs/Basic Core Programs/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Basic_Core_Programs.flipCoin run = new Basic_Core_Programs.flipCoin();
-run.flip();
+Console.WriteLine("Enter 1 to run flip Coin");
+Console.WriteLine("Enter 2 to run leap Year");
+int value = Convert.ToInt32(Console.ReadLine());
+
+
+switch (value)
+{
+    case 1:
+        Basic_Core_Programs.flipCoin run1 = new Basic_Core_Programs.flipCoin();
+        run1.flip();
+        break;
+    case 2:
+        Basic_Core_Programs.leapYear run2 = new Basic_Core_Programs.leapYear();
+        run2.checkLeapYear();
+        break;
+}
 
