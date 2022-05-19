6f7b38d49183ad1ca9c9c22da9bfb9ffc86a13a3
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-19 12:37:23 +0530

    Power of 2 added

--- Basic Core Programs/Basic Core Programs/Program.cs
+++ Basic Core Programs/Basic Core Programs/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Enter 1 to run flip Coin");
 Console.WriteLine("Enter 2 to run leap Year");
+Console.WriteLine("Enter 3 to run power of two program");
 int value = Convert.ToInt32(Console.ReadLine());
 
 
@@ -13,6 +14,10 @@ switch (value)
     case 2:
         Basic_Core_Programs.leapYear run2 = new Basic_Core_Programs.leapYear();
         run2.checkLeapYear();
+        break;
+    case 3:
+        Basic_Core_Programs.powerOf2 run3 = new Basic_Core_Programs.powerOf2();
+        run3.findPowerOf2();
         break;
 }
 
---
+++ Basic Core Programs/Basic Core Programs/powerOf2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    public class powerOf2
+    {
+        public void findPowerOf2()
+        {
+            int initialValue = 2;
+            Console.WriteLine("Enter values from 0 to 30");
+            int power = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= power; i++)
+            {
+                initialValue = initialValue * 2;
+            }
+            Console.WriteLine("2 power of " + power + " is " + initialValue);
+        }
+    }
+}
